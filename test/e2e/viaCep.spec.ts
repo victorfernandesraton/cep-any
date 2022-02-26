6903bf87e2e5170286c2fd4e4d1235eaae5586ab
victorfernandesraton <vfbraton@gmail.com>
2022-02-26 09:45:11 -0300

    fix cep eror works locally

--- test/e2e/viaCep.spec.ts
+++ test/e2e/viaCep.spec.ts
@@ -1,3 +1,4 @@
+import { RequestError } from "../../src/errors/requestError";
 import { ViaCepService } from "../../src/service/viacep/index";
 
 describe("ViaCepService", () => {
@@ -6,5 +7,16 @@ describe("ViaCepService", () => {
   test("should be a valid cep", async () => {
     const result = await stub.execute("41342320");
     expect(result.state).toStrictEqual("BA");
+  });
+  test("should be a valid cep with -", async () => {
+    const result = await stub.execute("41342-320");
+    expect(result.state).toStrictEqual("BA");
+    expect(result.city).toStrictEqual("Salvador");
+  });
+
+  test("should be not a valid cep", async () => {
+    expect(async () => {
+      await stub.execute("00000000");
+    }).rejects.toThrowError(new RequestError("not found", stub.api));
   });
 });
