2ea63824d4a8f401b5ab976d7d30cc5b551bd956
victorfernandesraton <vfbraton@gmail.com>
2022-03-04 10:12:54 -0300

    fix lib

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,101 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var tslib = require('tslib');
 var axios = require('axios');
 
 function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
 var axios__default = /*#__PURE__*/_interopDefaultLegacy(axios);
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    if (typeof b !== "function" && b !== null)
+        throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+}
+
+function __spreadArray(to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+}
 
 var Provider = (function () {
     function Provider(services) {
@@ -22,9 +111,9 @@ var Provider = (function () {
             enumerable: true,
             configurable: true,
             writable: true,
-            value: function (cep) { return tslib.__awaiter(_this, void 0, void 0, function () {
+            value: function (cep) { return __awaiter(_this, void 0, void 0, function () {
                 var result;
-                return tslib.__generator(this, function (_a) {
+                return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0:
                             _a.trys.push([0, 2, , 3]);
@@ -46,7 +135,7 @@ var Provider = (function () {
 }());
 
 var BasicError = (function (_super) {
-    tslib.__extends(BasicError, _super);
+    __extends(BasicError, _super);
     function BasicError() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
@@ -54,7 +143,7 @@ var BasicError = (function (_super) {
 }(Error));
 
 var RequestError = (function (_super) {
-    tslib.__extends(RequestError, _super);
+    __extends(RequestError, _super);
     function RequestError(message, api, request) {
         var _this = _super.call(this, message) || this;
         Object.defineProperty(_this, "api", {
@@ -77,7 +166,7 @@ var RequestError = (function (_super) {
 }(BasicError));
 
 var ParamError = (function (_super) {
-    tslib.__extends(ParamError, _super);
+    __extends(ParamError, _super);
     function ParamError(args) {
         return _super.call(this, "invalid params ".concat(args)) || this;
     }
@@ -103,9 +192,9 @@ var CepService = (function () {
             enumerable: true,
             configurable: true,
             writable: true,
-            value: function (cep) { return tslib.__awaiter(_this, void 0, void 0, function () {
+            value: function (cep) { return __awaiter(_this, void 0, void 0, function () {
                 var value, response;
-                return tslib.__generator(this, function (_a) {
+                return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0:
                             value = this.generalParse(cep);
@@ -153,16 +242,16 @@ function responseToCep$3(data) {
 }
 
 var ApiCepService = (function (_super) {
-    tslib.__extends(ApiCepService, _super);
+    __extends(ApiCepService, _super);
     function ApiCepService() {
         var _this = _super.call(this, "apicep") || this;
         Object.defineProperty(_this, "handler", {
             enumerable: true,
             configurable: true,
             writable: true,
-            value: function (cep) { return tslib.__awaiter(_this, void 0, void 0, function () {
+            value: function (cep) { return __awaiter(_this, void 0, void 0, function () {
                 var requestData, data, error_1;
-                return tslib.__generator(this, function (_a) {
+                return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0:
                             _a.trys.push([0, 3, , 4]);
@@ -203,20 +292,20 @@ var ApiCepService = (function (_super) {
 }(CepService));
 
 function responseToCep$2(data) {
-    return tslib.__assign({}, data);
+    return __assign({}, data);
 }
 
 var BrasilAPIService = (function (_super) {
-    tslib.__extends(BrasilAPIService, _super);
+    __extends(BrasilAPIService, _super);
     function BrasilAPIService() {
         var _this = _super.call(this, "brasilAPI") || this;
         Object.defineProperty(_this, "handler", {
             enumerable: true,
             configurable: true,
             writable: true,
-            value: function (cep) { return tslib.__awaiter(_this, void 0, void 0, function () {
+            value: function (cep) { return __awaiter(_this, void 0, void 0, function () {
                 var requestData, data, error_1;
-                return tslib.__generator(this, function (_a) {
+                return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0:
                             _a.trys.push([0, 3, , 4]);
@@ -261,7 +350,7 @@ var BrasilAPIService = (function (_super) {
 }(CepService));
 
 var ParserError = (function (_super) {
-    tslib.__extends(ParserError, _super);
+    __extends(ParserError, _super);
     function ParserError(message, api) {
         var _this = _super.call(this, message) || this;
         Object.defineProperty(_this, "api", {
@@ -315,16 +404,16 @@ function responseToCep$1(data) {
 }
 
 var CorreiosService = (function (_super) {
-    tslib.__extends(CorreiosService, _super);
+    __extends(CorreiosService, _super);
     function CorreiosService() {
         var _this = _super.call(this, "correios") || this;
         Object.defineProperty(_this, "handler", {
             enumerable: true,
             configurable: true,
             writable: true,
-            value: function (cep) { return tslib.__awaiter(_this, void 0, void 0, function () {
+            value: function (cep) { return __awaiter(_this, void 0, void 0, function () {
                 var requestData, data, error_1;
-                return tslib.__generator(this, function (_a) {
+                return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0:
                             _a.trys.push([0, 3, , 4]);
@@ -376,7 +465,7 @@ function responseToCep(data) {
 }
 
 var ViaCepService = (function (_super) {
-    tslib.__extends(ViaCepService, _super);
+    __extends(ViaCepService, _super);
     function ViaCepService() {
         var _this = _super.call(this, "viacep") || this;
         Object.defineProperty(_this, "baseUrl", {
@@ -389,9 +478,9 @@ var ViaCepService = (function (_super) {
             enumerable: true,
             configurable: true,
             writable: true,
-            value: function (cep) { return tslib.__awaiter(_this, void 0, void 0, function () {
+            value: function (cep) { return __awaiter(_this, void 0, void 0, function () {
                 var requestData, data, result, error_1;
-                return tslib.__generator(this, function (_a) {
+                return __generator(this, function (_a) {
                     switch (_a.label) {
                         case 0:
                             _a.trys.push([0, 2, , 3]);
@@ -440,7 +529,7 @@ function Factory(_a) {
         ];
     }
     if (custonProviders === null || custonProviders === void 0 ? void 0 : custonProviders.length) {
-        services = tslib.__spreadArray(tslib.__spreadArray([], services, true), custonProviders, true);
+        services = __spreadArray(__spreadArray([], services, true), custonProviders, true);
     }
     return new Provider(services);
 }
