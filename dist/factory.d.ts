9ef86bb515440553b3125b2411e1dfb536ba7f4e
Victor Raton <vfbraton@gmail.com>
2022-06-20 21:57:09 -0300

    fix version developed by

--- dist/factory.d.ts
+++ dist/factory.d.ts
@@ -1,8 +1,8 @@
 import { Provider } from "./provider";
 import { CepService } from "./service/index";
-interface FactoryOptions {
+declare type FactoryOptions = {
     useDefaultProviders?: boolean;
     custonProviders?: CepService[];
-}
+};
 export declare function Factory({ useDefaultProviders, custonProviders, }: FactoryOptions): Provider;
 export {};
