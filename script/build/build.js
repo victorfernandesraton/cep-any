bbd2d5a87a7f0eec5b103ce522b3992bd90a57f9
victorfernandesraton <vfbrato@gmail.com>
2022-08-18 00:04:49 -0300

    udpate libs

--- script/build/build.js
+++ script/build/build.js
@@ -3,8 +3,6 @@ require("esbuild").buildSync({
 	bundle: true,
 	minify: true,
 	platform: "neutral",
-	target: "esnext",
 	sourcemap: false,
-	// target: ["chrome58", "firefox57", "safari11", "edge16"],
 	outfile: "./dist/esm/index.mjs",
 })
