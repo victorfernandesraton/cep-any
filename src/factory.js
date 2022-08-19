cc7711bf9710a2793d8c556ae356b0c0a10e68e5
victorfernandesraton <vfbrato@gmail.com>
2022-08-18 22:25:17 -0300

    feat: override requester for custon requester in factory

--- src/factory.js
+++ src/factory.js
@@ -1,13 +1,24 @@
 import { Provider } from "./provider.js"
+import { Requester } from "./requester/index.js"
 import { ApiCepService } from "./service/apicep/index.js"
 import { BrasilAPIService } from "./service/brasilAPI/index.js"
 import { CorreiosService } from "./service/correios/index.js"
 import { ViaCepService } from "./service/viacep/index.js"
 
 
+/**
+ *
+ * @param {{
+ * useDefaultProviders?: boolean,
+ * custonProviders?: CepService,
+ * requester?: Requester
+ * }} param0
+ * @returns
+ */
 export default function ({
 	useDefaultProviders = true,
 	custonProviders,
+	requester = Requester
 }) {
 	let services = []
 	if (useDefaultProviders) {
@@ -20,6 +31,13 @@ export default function ({
 	}
 	if (custonProviders?.length) {
 		services = [...services, ...custonProviders]
+	}
+	if (requester) {
+		services = [...services.map(service => {
+			service.requester = requester
+			return service
+		})
+		]
 	}
 
 	return new Provider(services)
