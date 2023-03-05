1c7378335e8a953c040ddb884b79d01f611279b5
victorfernandesraton <vfbraton@gmail.com>
2023-03-04 23:34:12 -0300

    feat: adjust for using node cjs import

---
+++ bin/cep-any.js
@@ -0,0 +1,12 @@
+const { argv, exit } = require('node:process')
+const { cep } = require('../dist/cjs/index.js');
+
+(async function main() {
+    const value = argv[2]
+    const data = await cep(value)
+    return data
+
+}())
+    .then(console.info)
+    .catch(console.error)
+    .finally(exit)
