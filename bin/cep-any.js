9e7efab97fcb7f6aa7252385d06a022ed86d4113
Victor Raton <vfbraton@gmail.com>
2024-01-27 03:32:07 -0300

    fix: error in import index.js in bin

--- bin/cep-any.js
+++ bin/cep-any.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 const { argv, exit } = require('node:process')
-const { cep } = require('../dist/cjs/index.js');
+const { cep } = require('../dist/index');
 
 (async function main() {
-    const value = argv[2]
-    const data = await cep(value)
-    return data
+	const value = argv[2]
+	const data = await cep(value)
+	return data
 
 }())
-    .then(console.info)
-    .catch(console.error)
-    .finally(exit)
+	.then(console.info)
+	.catch(console.error)
+	.finally(exit)
